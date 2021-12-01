0581f71097043ab239ba24bc8dbe64e8bb0856ce
mullen25312 <mail@benjaminjahn.de>
2021-12-01 17:38:31 +0100

    feat: Solve d01p1 and d01p2

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
+# advent of code website: https://adventofcode.com/2021
+# github: https://github.com/mullen25312/aoc2021
+
 import os
 import importlib
 
-dailyPuzzles = ["d00"]
+dailyPuzzles = ["d00", "d01"]
 
 if __name__ == "__main__":
 
     for module in dailyPuzzles:
-
+        # import daily module
         importedModule = importlib.import_module(module + ".dailyPuzzle")
         puzzle = importedModule.DailyPuzzle(os.path.join(module, "input.txt"))
+
+        # solve puzzle
         puzzle.parse()
         puzzle.part_one()
-        print(puzzle.part_one_result)
         puzzle.part_two()
-        print(puzzle.part_two_result)
 
+        # print results
+        print(f"####### {module:s} - results #######")
+        print(puzzle.part_one_result)
+        print(puzzle.part_two_result)
