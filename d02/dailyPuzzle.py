791b7ca8d93a66fa06c729ca19c709099b96be9e
mullen25312 <mail@benjaminjahn.de>
2021-12-02 17:11:53 +0100

    feat: Solve d02p1 and d01p2

---
+++ d02/dailyPuzzle.py
@@ -0,0 +1,53 @@
+from dxx.superDailyPuzzle import SuperDailyPuzzle
+
+from itertools import combinations
+import numpy as np
+
+
+class DailyPuzzle(SuperDailyPuzzle):
+    def __init__(self, data_path):
+        super().__init__(data_path)
+
+    def parse(self, **kwargs):
+        self.parsed = [(line.split(" ")[0], int(line.split(" ")[1])) for line in self.data.splitlines()]
+
+    def part_one(self, **kwargs):
+        pos, depth = 0, 0
+
+        for move in self.parsed:
+            # algebraic solution
+            pos += (move[0] == "forward") * move[1]
+            depth += ((move[0] == "down") - (move[0] == "up")) * move[1]
+
+            # case solution
+            # if move[0] == "forward":
+            #     pos += move[1]
+            # elif move[0] == "down":
+            #     depth += move[1]
+            # elif move[0] == "up":
+            #     depth -= move[1]
+            # else:
+            #     pass
+
+        self.part_one_result = pos * depth
+
+    def part_two(self, **kwargs):
+        pos, depth, aim = 0, 0, 0
+
+        for move in self.parsed:
+            # algebraic solution
+            aim += ((move[0] == "down") - (move[0] == "up")) * move[1]
+            pos += (move[0] == "forward") * move[1]
+            depth += (move[0] == "forward") * (aim * move[1])
+
+            # if move[0] == "forward":
+            #     pos += move[1]
+            #     depth += aim * move[1]
+            # elif move[0] == "down":
+            #     aim += move[1]
+            # elif move[0] == "up":
+            #     aim -= move[1]
+            # else:
+            #     pass
+
+        self.part_two_result = pos * depth
--- test_framework.py
+++ test_framework.py
@@ -62,33 +62,31 @@ class Tests_d00:
         assert self.puzzle.part_two_result == 32858450
 
 
-# class Testing(unittest.TestCase):
 class Tests_d01:
     importedModule = importlib.import_module("d01" + ".dailyPuzzle")
     puzzle = importedModule.DailyPuzzle(os.path.join("d01", "input.txt"))
-    # test_config: TestConfig
-    # puzzle: SuperDailyPuzzle
-
-    # def __init__(self, puzzle, config_path):
-    #     self.puzzle = puzzle
-    #     with open(config_path, "r") as file:
-    #         self.test_config = TestConfig(yaml.load(file, Loader=yaml.FullLoader))
-
-    # def setup(self):
-    #     pass
-
-    # def teardown(self):
-    #     pass
 
     def test_part_one(self):
         self.puzzle.parse()
         self.puzzle.part_one()
-
-        # assert True  # self.puzzle.parsed == self.test_config.parse_res
         assert self.puzzle.part_one_result == 1692
 
     def test_part_two(self):
         self.puzzle.parse()
         self.puzzle.part_two()
-        # assert True  # self.puzzle.parsed == self.test_config.parse_res
         assert self.puzzle.part_two_result == 1724
+
+
+class Tests_d02:
+    importedModule = importlib.import_module("d02" + ".dailyPuzzle")
+    puzzle = importedModule.DailyPuzzle(os.path.join("d02", "input.txt"))
+
+    def test_part_one(self):
+        self.puzzle.parse()
+        self.puzzle.part_one()
+        assert self.puzzle.part_one_result == 2120749
+
+    def test_part_two(self):
+        self.puzzle.parse()
+        self.puzzle.part_two()
+        assert self.puzzle.part_two_result == 2138382217
